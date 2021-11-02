6c6a7a5aaf7115ff65347d5e2cb369258fb0260b
Morten Andersen <morten@hjemmesider.dk>
2021-11-02 10:50:05 +0100

    Update shortcode.php
    Better grid and gap numbers

--- files/shortcode.php
+++ files/shortcode.php
@@ -6,7 +6,7 @@ function webspeed_personer($atts) {
   ob_start();
 
   // define attributes and their defaults
-  extract(shortcode_atts(array('grid' => 'g-d-3', 'type' =>'' ), $atts));
+  extract(shortcode_atts(array('grid' => '3', 'gap' => '2', 'type' =>'' ), $atts));
 
 
 if (!empty($type)) {
@@ -37,9 +37,31 @@ if (!empty($type)) {
 
 }
 
+if( $grid == 2 ) {
+    $grid_class = ' g-d-2 ';
+} elseif ( $grid == 3) {
+    $grid_class = ' g-d-3 ';
+} elseif ( $grid == 4) {
+    $grid_class = ' g-d-4 ';
+}else {
+    $grid_class = ' g-d-1 ';
+}
+
+if( $gap == 1 ) {
+  $gap_class = 'gap-1 ';
+  } elseif( $gap == 2 ) {
+    $gap_class = 'gap-2 ';
+    } elseif ( $gap == 3) {
+        $gap_class = 'gap-3 ';
+    } elseif ( $gap == 4) {
+        $gap_class = 'gap-4 ';
+    }else {
+    $gap_class = 'no-gap ';
+}
+
 
 if ( $loop->have_posts() ) {
-  echo '<div class="personer-shortcode grid ' . $grid . ' gap-2">';
+  echo '<div class="personer-shortcode grid ' . $grid_class . $gap_class . '">';
   while ( $loop->have_posts() ) : $loop->the_post();
     echo '<div id="post-id-' .get_the_ID(). '" class="person-item">';
       webspeed_person_img();
